f121dd31a91e8f6cc22712a5c4d7f169d5fad651
shakil miah <shakilmiahr55@gmail.com>
2024-04-01 13:04:40 +0600

    first commit

---
+++ lang/en/subcategory.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Sub-Category Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for Sub-Category CRUD operations.
+    |
+    */
+  
+    'subcategories' => 'Sub-Category',
+    'manage_your_subcategories' => 'Manage your sub-categories',
+    'all_your_subcategories' => 'All your sub-categories',
+    'note' => 'Note',
+    'subcategory_name' => 'Sub-Category name',
+    'short_description' => 'Short description',
+    'added_success' => 'Sub-Category added successfully',
+    'updated_success' => 'Sub-Category updated successfully',
+    'deleted_success' => 'Sub-Category deleted successfully',
+    'add_subcategory' => 'Add Sub-Category ',
+    'edit_subcategory ' => 'Edit Sub-Category' ,
+    'subcategory_name' => 'Sub-Category Name',
+    'category'=>'Category',
+
+];
